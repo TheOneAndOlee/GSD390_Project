54c2243025f89c261a19d56e61fb860a845f37e3
TheOneAndOlee <olee081206@gmail.com>
2024-11-14 23:51:33 -0600

    Updated MoveClick's RequireComponent Attribute

--- Assets/MoveClick.cs
+++ Assets/MoveClick.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(BoxCollider))]
+[RequireComponent(typeof(Transform))]
 public class MoveClick : MonoBehaviour
 {
     public GameObject objectToMove;
